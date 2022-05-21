85083d14345cc5ecb865bcfd158d13703decb917
Abdulmojeed Ayoola <mojeedayoola58@gmail.com>
2022-05-21 23:28:46 +0100

    Add files via upload

--- tech.js
+++ tech.js
@@ -4,7 +4,7 @@ const section = document.querySelector('section')
 
 
 section.addEventListener("click", (e)=>{
-    if(e.target.classList.contains("link-btn")) {
+    if(e.target.classList.contains("btn")) {
     let id = e.target.dataset.id
       fetchData(id)
     }
@@ -15,32 +15,29 @@ section.addEventListener("click", (e)=>{
       .then(res => res.json())
       .then(datas => {
           let data = Object.entries(datas)
-          let destinations = data[1][1]
+          let destinations = data[2][1]
   
           destinations.forEach((des)=>{
               if(des.name === id){
                   let element = `
                   <div class="content">
+        <h3>03 SPACE LAUNCH 101</h3>
         <article>
-          <h3>02 MEET YOUR CREW</h3>
-
+          <div class="buttons-btn">
+            <button data-id="Launch vehicle" class="btn">1</button>
+            <button data-id="Spaceport" class="btn">2</button>
+            <button data-id="Space capsule" class="btn">3</button>
+          </div>
           <div class="place">
-            <h2>${des.role}</h2>
+            <h2>The terminology...</h2>
             <h1>${des.name}</h1>
-            <p>${des.bio}</p>
-            <div class="buttons">
-            
-              <button data-id="Douglas Hurley" class="link-btn white"></button>
-              <button data-id="Mark Shuttleworth" class="link-btn"></button>
-              <button data-id="Victor Glover" class="link-btn"></button>
-              <button data-id="Anousheh Ansari" class="link-btn"></button>
-              </div>
+            <p>${des.description}</p>
             
           </div>
         </article>
       </div>
-        <div class="crew-image">
-          <img src="${des.images.png}" alt="">
+        <div class="tech-image">
+          <img src="${des.images.portrait}" alt="">
         </div>
                   `
                   return section.innerHTML = element
@@ -55,6 +52,6 @@ section.addEventListener("click", (e)=>{
   
   
   window.addEventListener("DOMContentLoaded", ()=>{
-    let id = "Douglas Hurley"
+    let id = "Launch vehicle"
     fetchData(id)
   })
